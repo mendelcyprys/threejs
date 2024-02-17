f3b516135beb80ed880c9cf4a89d49f115fe334a
Mendel Cyprys <mmcyprys@gmail.com>
2024-02-17 22:06:17 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const links = [
+    {
+        name: "Creating a scene",
+        link: "exercises/creating_a_scene/creating_a_scene.html",
+        tut_link: "https://threejs.org/docs/#manual/en/introduction/Creating-a-scene",
+    },
+    {
+        name: "Drawing lines",
+        link: "exercises/drawing_lines/drawing_lines.html",
+        tut_link: "https://threejs.org/docs/#manual/en/introduction/Drawing-lines",
+    },
+    {
+        name: "Creating text",
+        link: "exercises/creating_text/creating_text.html",
+        tut_link: "https://threejs.org/docs/#manual/en/introduction/Creating-text",
+    },
+]
+const list_element = document.querySelector("ul");
+links.forEach(({ name, link, tut_link }) => {
+    list_element.innerHTML += `
+        <li>
+            <a
+                href="${link}"
+                target="_blank"
+                class="text-orange-700 underline hover:text-orange-500"
+            >
+                ${name}
+            </a>
+            - tutorial
+            <a
+                href="${tut_link}"
+                target="_blank"
+                class="text-orange-700 underline hover:text-orange-500"
+            >
+                link
+            </a>
+        </li>
+    `;
+});
