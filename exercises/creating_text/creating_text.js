17019e3721f05c1bcef5c6efe0b8efb643336142
Mendel Cyprys <mmcyprys@gmail.com>
2024-02-19 03:41:47 +0000

    Stuff

---
+++ exercises/creating_text/creating_text.js
@@ -0,0 +1,19 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  500
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// TO DO stuff?
+
+renderer.render(scene, camera);
